28df93cbc48e8a207b9e7d56780f2b5d45304528
Aussiroth <aussiroth@gmail.com>
2017-06-04 20:24:02 +0800

    Version 1.0
    Initial game commit with randomised platform

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public PlayerController thePlayer;
+
+	private Vector3 lastPlayerPosition;
+	private float offset;
+	// Use this for initialization
+	void Start () {
+		thePlayer = FindObjectOfType<PlayerController> ();
+		lastPlayerPosition = thePlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset = thePlayer.transform.position.x - lastPlayerPosition.x;
+		transform.position = new Vector3 (transform.position.x + offset, transform.position.y, transform.position.z);
+		lastPlayerPosition = thePlayer.transform.position;
+	}
+}
---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject thePlatform;
+	public Transform generationPoint;
+	public float distanceBetween;
+	private float platformWidth;
+
+	public GameObject[] thePlatforms;
+	public int[] platformWidths;
+
+	public float distanceBetweenMin;
+	public float distanceBetweenMax;
+
+	private int platformChosen;
+	private float minHeight;
+	private float maxHeight;
+	public Transform maxHeightPoint;
+	public float maxHeightChange;
+	private float newHeight;
+
+	// Use this for initialization
+	void Start () {
+		platformWidth = thePlatform.GetComponent<BoxCollider2D> ().size.x;
+
+		minHeight = transform.position.y;
+		maxHeight = maxHeightPoint.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < generationPoint.position.x) {
+			//Decide which platform to make
+			platformChosen = Random.Range(0, thePlatforms.Length);
+			
+			distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+
+			//get new y coordinate of platform
+			newHeight = transform.position.y + Random.Range (-maxHeightChange, maxHeightChange);
+			if (newHeight > maxHeight)
+				newHeight = maxHeight;
+			if (newHeight < minHeight)
+				newHeight = minHeight;
+
+			transform.position = new Vector3 (transform.position.x + platformWidth + distanceBetween, newHeight, transform.position.z);
+
+			Instantiate (thePlatforms[platformChosen], transform.position, transform.rotation);
+		}
+		
+	}
+}
